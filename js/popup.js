b5ccae928531c170f51f728bb54a80870cbe1fc6
최재욱 <a90189829@gmail.com>
2022-02-08 22:00:13 +0900

    re

--- js/popup.js
+++ js/popup.js
@@ -4,10 +4,10 @@ const overlay = modal.querySelector(".product_overlay");
 const closeBtn = modal.querySelector(".product_outb");
 const openModal = () => {
     modal.classList.remove("product_hidden");
-}
+} 
 const closeModal = () => { 
     modal.classList.add("product_hidden");
-}
+}  
 overlay.addEventListener("click", closeModal);
 closeBtn.addEventListener("click", closeModal);
 openButton.addEventListener("click", openModal);
