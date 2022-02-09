9a8d78f3e070f7fc49a39d740564622d2dfedcc8
최재욱 <a90189829@gmail.com>
2022-02-09 23:58:00 +0900

    2022-02-09-11:57

--- js/popup.js
+++ js/popup.js
@@ -17,17 +17,20 @@ overlay.addEventListener("click", closeModal);
 // 닫기 버튼 클릭시 모달 닫기
 closeBtn.addEventListener("click", closeModal);
 
-
-
 // 사진에 전부 모달 여는 이벤트 달기
 for (var i = 0; i < openButton.length; i++ ){
     openButton.item(i).addEventListener("click", openModal);
 }
+
 // 사진에 맞는 모달창 열기
+
 $(document).ready(function(){
   $('.product_img').click(function(){
     modal.classList.remove("product_hidden");
     document.querySelector(".product_box .product_info img").src = $(this).children('img').attr("src");
+    document.querySelector(".product_box .info_ment span").innerTEXT = $(this).siblings('figcaption').children('span').text();
+    document.querySelector(".product_box .info_ment span").innerTEXT = $(this).siblings('figcaption').children('span').text();
     console.log($(this).children('img').attr("src"));
+    console.log($(this).siblings('figcaption').children('h1').attr('innerTEXT'));
   });
 });
